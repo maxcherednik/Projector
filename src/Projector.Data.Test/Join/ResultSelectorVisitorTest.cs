7a6f2c73cccb201ad7e89814d273067b141a9099
Maxim Cherednik <maxim.cherednik@gkfx.com>
2017-04-28 21:50:18 +0000

    ResultSelectorVisitor for Join implemented

---
+++ src/Projector.Data.Test/Join/ResultSelectorVisitorTest.cs
@@ -0,0 +1,158 @@
+﻿using NSubstitute;
+using Projector.Data.Join;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Projector.Data.Test.Join
+{
+    public class ResultSelectorVisitorTest
+    {
+        private ISchema _mockSchemaLeft;
+        private IField<int> _mockAgeField;
+        private IField<string> _mockNameField;
+
+        private ISchema _mockSchemaRight;
+        private IField<string> _mockStreetField;
+        private IField<int> _mockHouseNumberField;
+        private Dictionary<int, Tuple<int, int>> _rowMap;
+
+        public ResultSelectorVisitorTest()
+        {
+            // left schema setup
+            _mockSchemaLeft = Substitute.For<ISchema>();
+
+            _mockAgeField = Substitute.For<IField<int>>();
+            _mockAgeField.GetValue(125).Returns(25);
+
+            _mockNameField = Substitute.For<IField<string>>();
+            _mockNameField.GetValue(125).Returns("Max");
+
+            _mockSchemaLeft.GetField<int>("Age").Returns(_mockAgeField);
+
+            _mockSchemaLeft.GetField<string>("Name").Returns(_mockNameField);
+
+            // right schema setup
+            _mockSchemaRight = Substitute.For<ISchema>();
+
+            _mockHouseNumberField = Substitute.For<IField<int>>();
+            _mockHouseNumberField.GetValue(13).Returns(221);
+
+            _mockStreetField = Substitute.For<IField<string>>();
+            _mockStreetField.GetValue(13).Returns("Baker street");
+
+            _mockSchemaRight.GetField<int>("HouseNumber").Returns(_mockHouseNumberField);
+
+            _mockSchemaRight.GetField<string>("Street").Returns(_mockStreetField);
+
+            _rowMap = new Dictionary<int, Tuple<int, int>>
+            {
+                { 1, Tuple.Create(125, 13) }
+            };
+        }
+
+        [Fact]
+        public void CreateSimpleProjectionSchemaFromExpressionTest()
+        {
+            //call
+            Expression<Func<Person, PersonAddress, dynamic>> filterExpression = (person, personAddress) => new
+            {
+                Name = person.Name,
+                NameAge = person.Name + person.Age,
+                personAddress.Street,
+                NameAddressHouse = person.Name + personAddress.Street + personAddress.HouseNumber
+            };
+
+            var projectedFieldsMeta = new ResultSelectorVisitor().GenerateProjection(filterExpression);
+            var oldFieldNamesToNewFieldNamesMapping = projectedFieldsMeta.Item1;
+            var projectedFields = projectedFieldsMeta.Item2;
+
+            //check mappings
+
+            Assert.Equal(4, oldFieldNamesToNewFieldNamesMapping.Count);
+
+            Assert.True(oldFieldNamesToNewFieldNamesMapping.ContainsKey("Name"));
+            Assert.True(oldFieldNamesToNewFieldNamesMapping.ContainsKey("Age"));
+            Assert.True(oldFieldNamesToNewFieldNamesMapping.ContainsKey("Street"));
+            Assert.True(oldFieldNamesToNewFieldNamesMapping.ContainsKey("HouseNumber"));
+
+            var firstMapping = oldFieldNamesToNewFieldNamesMapping["Name"];
+            Assert.Equal(3, firstMapping.Count);
+            Assert.True(firstMapping.Contains("Name"));
+            Assert.True(firstMapping.Contains("NameAge"));
+            Assert.True(firstMapping.Contains("NameAddressHouse"));
+
+
+            var secondMapping = oldFieldNamesToNewFieldNamesMapping["Age"];
+            Assert.Equal(1, secondMapping.Count);
+            Assert.True(secondMapping.Contains("NameAge"));
+
+            var thirdMapping = oldFieldNamesToNewFieldNamesMapping["Street"];
+            Assert.Equal(2, thirdMapping.Count);
+            Assert.True(thirdMapping.Contains("Street"));
+            Assert.True(thirdMapping.Contains("NameAddressHouse"));
+
+            var fouthMapping = oldFieldNamesToNewFieldNamesMapping["HouseNumber"];
+            Assert.Equal(1, fouthMapping.Count);
+            Assert.True(fouthMapping.Contains("NameAddressHouse"));
+
+            // check projected fields
+
+            Assert.Equal(4, projectedFields.Count);
+            Assert.True(projectedFields.ContainsKey("Name"));
+            Assert.True(projectedFields.ContainsKey("NameAge"));
+            Assert.True(projectedFields.ContainsKey("Street"));
+            Assert.True(projectedFields.ContainsKey("NameAddressHouse"));
+
+
+            var nameField = (JoinProjectedField<string>)projectedFields["Name"];
+            nameField.SetLeftSchema(_mockSchemaLeft);
+            nameField.SetRightSchema(_mockSchemaRight);
+            nameField.SetRowIdMap(_rowMap);
+
+            Assert.Equal("Max", nameField.GetValue(1));
+
+
+            var nameAgeField = (JoinProjectedField<string>)projectedFields["NameAge"];
+            nameAgeField.SetLeftSchema(_mockSchemaLeft);
+            nameAgeField.SetRightSchema(_mockSchemaRight);
+            nameAgeField.SetRowIdMap(_rowMap);
+
+            Assert.Equal("Max25", nameAgeField.GetValue(1));
+
+
+            var streetField = (JoinProjectedField<string>)projectedFields["Street"];
+            streetField.SetLeftSchema(_mockSchemaLeft);
+            streetField.SetRightSchema(_mockSchemaRight);
+            streetField.SetRowIdMap(_rowMap);
+
+            Assert.Equal("Baker street", streetField.GetValue(1));
+
+
+            var nameAddressHouseField = (JoinProjectedField<string>)projectedFields["NameAddressHouse"];
+            nameAddressHouseField.SetLeftSchema(_mockSchemaLeft);
+            nameAddressHouseField.SetRightSchema(_mockSchemaRight);
+            nameAddressHouseField.SetRowIdMap(_rowMap);
+
+            Assert.Equal("MaxBaker street221", nameAddressHouseField.GetValue(1));
+        }
+
+        private class Person
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+
+        private class PersonAddress
+        {
+            public string Name { get; set; }
+
+            public string Street { get; set; }
+
+            public int HouseNumber { get; set; }
+        }
+
+    }
+}
--- src/Projector.Data/Join/ResultSelectorVisitor.cs
+++ src/Projector.Data/Join/ResultSelectorVisitor.cs
@@ -5,20 +5,30 @@ using System.Reflection;
 
 namespace Projector.Data.Join
 {
-    class ResultSelectorVisitor : ExpressionVisitor
+    public class ResultSelectorVisitor : ExpressionVisitor
     {
         private string _currentProjectedName;
 
         private IDictionary<string, IField> _projectedFields;
         private IDictionary<string, ISet<string>> _oldFieldNamesToNewFieldNamesMapping;
-        private ParameterExpression _schemaParameter;
-        private ParameterExpression _idParameter;
-        private object _getFieldMethodInfo;
+        private ParameterExpression _schemaParameterLeft;
+        private ParameterExpression _idParameterLeft;
+        private ParameterExpression _schemaParameterRight;
+        private ParameterExpression _idParameterRight;
+        private MethodInfo _getFieldMethodInfo;
+        private bool _skip;
+
+        private Dictionary<ParameterExpression, Tuple<ParameterExpression, ParameterExpression>> _parametersToScheamMap;
 
         public ResultSelectorVisitor()
         {
-            _schemaParameter = Expression.Parameter(typeof(ISchema), "schema");
-            _idParameter = Expression.Parameter(typeof(int), "id");
+            _schemaParameterLeft = Expression.Parameter(typeof(ISchema), "schema");
+            _idParameterLeft = Expression.Parameter(typeof(int), "id");
+
+            _schemaParameterRight = Expression.Parameter(typeof(ISchema), "schema");
+            _idParameterRight = Expression.Parameter(typeof(int), "id");
+
+            _parametersToScheamMap = new Dictionary<ParameterExpression, Tuple<ParameterExpression, ParameterExpression>>(2);
 
             _getFieldMethodInfo = typeof(ISchema).GetTypeInfo().GetDeclaredMethod("GetField");
             _projectedFields = new Dictionary<string, IField>();
@@ -34,6 +44,14 @@ namespace Projector.Data.Join
             return Tuple.Create(_oldFieldNamesToNewFieldNamesMapping, _projectedFields);
         }
 
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            _parametersToScheamMap.Add(node.Parameters[0], Tuple.Create(_schemaParameterLeft, _idParameterLeft));
+            _parametersToScheamMap.Add(node.Parameters[1], Tuple.Create(_schemaParameterRight, _idParameterRight));
+
+            return base.VisitLambda(node);
+        }
+
         private void GenerateField(string projectedFieldName, Type typeOfValue, Expression expression)
         {
             _currentProjectedName = projectedFieldName;
@@ -41,11 +59,70 @@ namespace Projector.Data.Join
 
             var typeOfFunc = typeof(Func<,,,,>).MakeGenericType(typeof(ISchema), typeof(int), typeof(ISchema), typeof(int), typeOfValue);
 
-            var lambda = Expression.Lambda(typeOfFunc, Visit(expression), _schemaParameter, _idParameter);
+            var lambda = Expression.Lambda(typeOfFunc, Visit(expression), _schemaParameterLeft, _idParameterLeft, _schemaParameterRight, _idParameterRight);
 
             var projectedField = Activator.CreateInstance(projectedFieldType, projectedFieldName, lambda.Compile());
 
             _projectedFields.Add(projectedFieldName, (IField)projectedField);
+        }
+
+        protected override MemberAssignment VisitMemberAssignment(MemberAssignment node)
+        {
+            var projectedFieldName = node.Member.Name;
+            var typeOfvalue = node.Expression.Type;
+
+            GenerateField(projectedFieldName, typeOfvalue, node.Expression);
+
+            return node;
+        }
+
+        protected override Expression VisitNew(NewExpression node)
+        {
+            // this is for anonymous types only
+            if (node.Members != null)
+            {
+                for (int i = 0; i < node.Members.Count; i++)
+                {
+                    var member = node.Members[i];
+                    var valueExpression = node.Arguments[i];
+                    var projectedFieldName = member.Name;
+                    var typeOfvalue = valueExpression.Type;
+
+                    GenerateField(projectedFieldName, typeOfvalue, valueExpression);
+                }
+
+                _skip = true;
+            }
+
+            return base.VisitNew(node);
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            if (_skip)
+            {
+                return base.VisitMember(node);
+            }
+
+            var schemaParameter = _parametersToScheamMap[(ParameterExpression)node.Expression].Item1;
+            var idParameter = _parametersToScheamMap[(ParameterExpression)node.Expression].Item2;
+
+
+            var oldFieldName = node.Member.Name;
+            if (!_oldFieldNamesToNewFieldNamesMapping.TryGetValue(oldFieldName, out ISet<string> newFieldNames))
+            {
+                newFieldNames = new HashSet<string>();
+                _oldFieldNamesToNewFieldNamesMapping.Add(oldFieldName, newFieldNames);
+            }
+            newFieldNames.Add(_currentProjectedName);
+
+            var genericMethodInfo = _getFieldMethodInfo.MakeGenericMethod(node.Type);
+            var fieldAccessExpression = Expression.Call(schemaParameter, genericMethodInfo, Expression.Constant(oldFieldName, typeof(string)));
+
+            var valueAccessMemberInfo = genericMethodInfo.ReturnType.GetTypeInfo().GetDeclaredMethod("GetValue");
+
+            var valueAccessExpression = Expression.Call(fieldAccessExpression, valueAccessMemberInfo, idParameter);
+            return valueAccessExpression;
         }
     }
 }
